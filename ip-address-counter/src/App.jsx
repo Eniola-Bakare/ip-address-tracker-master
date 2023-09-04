a730e0e0b57f7a1632eb508abb5646299d2b4c30
Eniola Bakare <eebakare@gmail.com>
2023-09-04 19:59:37 +0100

    Building the static app

---
+++ ip-address-counter/src/App.jsx
@@ -0,0 +1,11 @@
+import Map from "./components/Map";
+function App() {
+  return (
+    <main>
+      <header></header>
+      <Map />
+    </main>
+  );
+}
+
+export default App;
---
+++ ip-address-counter/src/components/Map.jsx
@@ -0,0 +1,9 @@
+function Map() {
+  return (
+    <div>
+      <p>I'm a map!</p>
+    </div>
+  )
+}
+
+export default Map
