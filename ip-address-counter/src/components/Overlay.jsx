232146a7319edb0129329094d004952f14bcddeb
Eniola Bakare <eebakare@gmail.com>
2023-09-08 12:09:10 +0100

    Documentation

--- ip-address-counter/src/components/Overlay.jsx
+++ ip-address-counter/src/components/Overlay.jsx
@@ -3,7 +3,7 @@ import Search from "./Search";
 
 function Overlay() {
   return (
-    <div className="flex flex-col gap-10 items-center w-full absolute -bottom-28 sm:absolute sm:-bottom-12">
+    <div className="flex flex-col gap-10 items-center w-full absolute -bottom-28 sm:absolute sm:-bottom-12 ">
       <Search />
       <Details />
     </div>
